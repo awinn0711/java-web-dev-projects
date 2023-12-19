4fb5acbb744d77b3067dbf030678d3e917204bec
Austin Winn <awinn0711@gmail.com>
2023-12-18 19:32:29 -0600

    completed studio

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -7,6 +7,51 @@ import static org.junit.jupiter.api.Assertions.*;
 class BalancedBracketsTest {
     //TODO: add tests here
     @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void charactersInBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[abc]"));
+    }
+    @Test
+    public void oneOpenBracketReturnsFalse() {
+
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[f"));
+    }
+    @Test
+    public void oneClosedBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("f]"));
+    }
+    @Test
+    public void moreOpenBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]]]"));
+    }
+    @Test
+    public void moreClosedBracketsReturnsFalse() {
+        assertFalse((BalancedBrackets.hasBalancedBrackets("[[[]]")));
+    }
+    @Test
+    public void oneOpenBracketWithMultipleCharactersReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[abc"));
+    }
+    @Test
+    public void onlyBracketsBackwardsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void oppositeBracketsWithMultipleCharactersReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]abcd["));
+    }
+    @Test
+    public void multipleOppositeBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]][[["));
+    }
+    @Test
+    public void oppositeBracketsAfterCorrectBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[][]]["));
+    }
+    @Test
     public void emptyTest() {
         assertEquals(true, true);
     }
